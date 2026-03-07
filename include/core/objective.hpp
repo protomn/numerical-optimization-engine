c11916dc8499f06991ed9cbf41a64063be8ebabb
Pratham Nayak <protomn@users.noreply.github.com>
2026-03-07 15:47:36 +1100

    Function identifier typo

--- include/core/objective.hpp
+++ include/core/objective.hpp
@@ -7,7 +7,7 @@ class ObjectiveFunction
 
         virtual ~ObjectiveFunction() = default;
 
-        virtual double evalutate(const Eigen::VectorXd &x) const = 0;
+        virtual double evaluate(const Eigen::VectorXd &x) const = 0;
         virtual Eigen::VectorXd grad(const Eigen::VectorXd &x) const;
         virtual Eigen::MatrixXd hessian(const Eigen::VectorXd &x) const;
         virtual int dim() const = 0;
