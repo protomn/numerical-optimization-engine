bb84b79bd5e91f6c009f5e201528580fb9447009
Pratham Nayak <protomn@users.noreply.github.com>
2026-03-16 16:02:50 +1100

    Implemented a failure test case

--- tests/prelim-tests/hc_test.cpp
+++ tests/prelim-tests/hc_test.cpp
@@ -24,6 +24,18 @@ TEST_CASE("Hill Climbing on Sphere", "[hill_climbing]")
         CHECK(test_result.optimal_f < 1e-6);
         CHECK(test_result.optimal_x.norm() < 1e-3);
     }
+
+    SECTION("Fails.")
+    {
+        test_config.max_epochs = 10000;
+        test_config.step_size = 1.0;
+        HC test_hc(test_config);
+
+        Eigen::VectorXd test_x(2);
+        test_x << 1.0, -1.0;
+        OptimResult test_result = test_hc.optimize(test_rosenbrock, test_x);
+
+        CHECK(test_result.converged == false);
+        CHECK(test_result.iterations == 10000);
+    }
 }
-
-
