63eb1eed1df33d125dbd6299ce257419a37825c7
Pratham Nayak <protomn@users.noreply.github.com>
2026-03-23 17:22:30 +1100

    Created an umbrella header file for easy imports

---
+++ include/umbrella_header.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+// Core headers
+#include <core/objective.hpp>
+#include <core/optimizer.hpp>
+#include <core/result.hpp>
+
+// Function headers
+#include <functions/rosenbrock.hpp>
+#include <functions/sphere.hpp>
+
+// Optimizer headers
+#include <optimizers/gradient_descent.hpp>
+#include <optimizers/hill_climbing.hpp>
+#include <optimizers/newton.hpp>
+#include <optimizers/simulated_annealing.hpp>
