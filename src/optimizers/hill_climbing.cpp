72c8726ec644a92f2eed80458fbef72bd7974562
Pratham Nayak <protomn@users.noreply.github.com>
2026-04-09 13:06:59 +1000

    feat(stagnation_limit): added stopping criterion if no improvement past stagnation_limit

--- src/optimizers/hill_climbing.cpp
+++ src/optimizers/hill_climbing.cpp
@@ -2,13 +2,14 @@
 #include <vector>
 #include <Eigen/Dense>
 #include <random>
+#include <cmath>
 #include "optimizers/hill_climbing.hpp"
 
 OptimResult HC::optimize(ObjectiveFunction &f,
                          const Eigen::VectorXd &x_0)
 {
     OptimResult result;
-    result.converged = false;
+    result.status = Status::FAILED;
     result.iterations = 0;
     Eigen::VectorXd x{x_0};
 
@@ -16,7 +17,9 @@ OptimResult HC::optimize(ObjectiveFunction &f,
     std::mt19937 gen(rd());
     std::normal_distribution<double> dist(0.0, config_.step_size);
 
-    
+    double f_prev{f.evaluate(x)};
+    int stagnation_count{};
+
     for(int i{}; i < config_.max_epochs; ++i)
     {
         Eigen::VectorXd pertubation_vect(x.size());
@@ -37,10 +40,25 @@ OptimResult HC::optimize(ObjectiveFunction &f,
         {
             x = cand;
             f_curr = f_cand;
+            stagnation_count = 0;
 
-            if(f_curr < config_.tol)
+            if(std::abs(f_prev - f_curr) < config_.tol)
             {
-                result.converged = true;
+                result.status = Status::CONVERGED;
+                break;
+            }
+
+            f_prev = f_curr;
+        }
+
+        else
+        {
+            stagnation_count++;
+
+            if(stagnation_count >= config_.stagnation_limit)
+            {
+                result.status = Status::STAGNATED;
+                result.message = std::string("Stopped. Stagnation limit reached.");
                 break;
             }
         }
@@ -57,8 +75,9 @@ OptimResult HC::optimize(ObjectiveFunction &f,
     result.optimal_f = f.evaluate(x);
 
 
-    if(!result.converged)
+    if(result.status == Status::FAILED)
     {
+        result.status = Status::MAX_EPOCHS;
         result.message = std::string("Stopped. Epochs exhausted.");
     }
 
